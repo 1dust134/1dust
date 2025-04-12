1c586c8fe48163108e5b9343433c07f20d6ce7d6
808 <a808@808deMacBook-Pro.local>
2025-04-12 14:13:36 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const isActive = ref(false)
+</script>
+
+<template>
+  <div>
+    <button @click="isActive = !isActive">切换样式</button>
+    <p :class="{ active: isActive }">这是一段动态样式的文字</p>
+  </div>
+</template>
+
+<style>
+.active {
+  color: purple;
+  font-weight: bold;
+}
+</style>
